f1242980c807b30b829e53a6518e01d04e631038
William Harrison <william@williamdavidharrison.com.au>
2024-01-01 10:47:38 +0800

    Update install-docker.sh

--- ubuntu/install-docker.sh
+++ ubuntu/install-docker.sh
@@ -1,10 +1,12 @@
 # https://docs.docker.com/engine/install/ubuntu/
 
 # Uninstall old versions
+echo "Uninstalling old Docker versions..."
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
 
 # Install using the apt repository
 # Add Docker's official GPG key:
+echo "Adding Docker's official GPG key..."
 sudo apt-get update -y
 sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -12,6 +14,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
+echo "Adding Docker's repository to Apt sources..."
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
@@ -19,6 +22,7 @@ echo \
 sudo apt-get update -y
 
 # Install the Docker packages
+echo "Installing Docker packages..."
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Complete
