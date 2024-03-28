7c812c710e683c1d4b05d565de9b4809f6fd6692
William Harrison <william@williamdavidharrison.com.au>
2024-03-28 08:25:27 +0800

    Update install-docker.sh

--- ubuntu/install-docker.sh
+++ ubuntu/install-docker.sh
@@ -8,15 +8,15 @@ for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker c
 # Add Docker's official GPG key:
 echo "Adding Docker's official GPG key..."
 sudo apt-get update -y
-sudo apt-get install -y ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo "Adding Docker's repository to Apt sources..."
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
