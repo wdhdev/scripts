81afa9cd5e720b1573e793257d9315d5a929c010
William Harrison <william@williamdavidharrison.com.au>
2024-03-28 08:25:59 +0800

    Update install-docker.sh

--- debian/install-docker.sh
+++ debian/install-docker.sh
@@ -2,13 +2,13 @@
 
 # Uninstall old versions
 echo "Uninstalling old Docker versions..."
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
 
 # Install using the apt repository
 # Add Docker's official GPG key:
 echo "Adding Docker's official GPG key..."
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -19,11 +19,11 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 
 # Install the Docker packages
 echo "Installing Docker packages..."
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Complete
 echo "Docker install complete."
