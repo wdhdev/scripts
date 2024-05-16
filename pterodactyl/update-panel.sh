80c4e1947df396e4a28fb0c1c34a035169cdf675
William Harrison <william@williamdavidharrison.com.au>
2024-05-16 18:55:39 +0800

    Update update-panel.sh

--- pterodactyl/update-panel.sh
+++ pterodactyl/update-panel.sh
@@ -2,6 +2,7 @@ cd /var/www/pterodactyl
 php artisan down
 curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
 chmod -R 755 storage/* bootstrap/cache
+export COMPOSER_ALLOW_SUPERUSER=1;
 composer install --no-dev --optimize-autoloader
 php artisan view:clear
 php artisan config:clear
