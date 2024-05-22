e662add1e7068871425fcae7ae23f756e7774f83
William Harrison <william@williamdavidharrison.com.au>
2024-05-22 09:27:13 +0800

    Create update-wings.sh

---
+++ pterodactyl/update-wings.sh
@@ -0,0 +1,4 @@
+systemctl stop wings
+curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+chmod u+x /usr/local/bin/wings
+systemctl restart wings
